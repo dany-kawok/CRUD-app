3e032987c258471eb473959494690c98f7b243f8
dany-kawok <dany.kawok@gmail.com>
2024-05-31 20:24:22 +0300

    cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,5 @@
 const { model } = require("mongoose");
 
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000", "https://lms-plum.vercel.app"];
+
 module.exports = allowedOrigins;
